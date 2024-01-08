ed3ef76bb704ea001bb814b6dab330338c1b296c
songhyunseop <gustjq99455352@gmail.com>
2024-01-09 00:37:26 +0900

    :Backend API 구현 및 Postman 테스트

---
+++ todo_List/todo_backend/controller/task.controller.js
@@ -0,0 +1,56 @@
+const Task = require('../model/task');
+
+const taskController = {};
+
+// 생성 API
+taskController.createTask = async (req, res) => {
+  try {
+    const { task, isComplete } = req.body;
+    const newTask = new Task({ task, isComplete });
+    await newTask.save();
+    res.status(200).json({ status: 'ok', data: newTask });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', error });
+  }
+};
+
+// 조회 API
+taskController.getTask = async (req, res) => {
+  try {
+    const taskList = await Task.find({}).select('-__v');
+    res.status(200).json({ status: 'ok', data: taskList });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', error });
+  }
+};
+
+// 수정 API
+taskController.updateTask = async (req, res) => {
+  try {
+    const { task, isComplete } = req.body;
+    const updateTask = await Task.findById(req.params.id);
+
+    updateTask.task = task;
+    updateTask.isComplete = isComplete;
+
+    await updateTask.save();
+
+    res.status(200).json({ status: 'ok', data: updateTask });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', error });
+  }
+};
+
+// 삭제 API
+taskController.deleteTask = async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(req.params.id);
+    if (!deletedTask) throw new Error('there is no Task');
+
+    res.status(200).json({ status: 'ok', data: deletedTask });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', error: error.message });
+  }
+};
+
+module.exports = taskController;
--- todo_List/todo_backend/routes/task.api.js
+++ todo_List/todo_backend/routes/task.api.js
@@ -1,24 +1,17 @@
 const express = require('express');
+const taskController = require('../controller/task.controller');
 const router = express.Router();
 
 // 할 일 추가
-router.post('/', (req, res) => {
-  res.send('create Task');
-});
+router.post('/', taskController.createTask);
 
 // 할 일 리스트 조회 (View)
-router.get('/', (req, res) => {
-  res.send('get Task');
-});
+router.get('/', taskController.getTask);
 
 // 할 일 수정
-router.put('/:id', (req, res) => {
-  res.send('update Task');
-});
+router.put('/:id', taskController.updateTask);
 
 // 할 일 삭제
-router.delete('/:id', (req, res) => {
-  res.send('delete Task');
-});
+router.delete('/:id', taskController.deleteTask);
 
 module.exports = router;
