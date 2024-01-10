3f241793386fb92e7f78e934a354664498dc998a
songhyunseop <gustjq99455352@gmail.com>
2024-01-11 02:18:20 +0900

    백엔드 환경변수 설정 및 서버 배포완료

--- todo_List/todo_backend/app.js
+++ todo_List/todo_backend/app.js
@@ -4,19 +4,21 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 
+require('dotenv').config();
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', indexRouter);
 
-const dbUri = 'mongodb://localhost:27017/toDo3';
+const dbUri = process.env.MONGODB_URI_PROD;
 
 mongoose
   .connect(dbUri)
   .then(() => console.log('mongoose Connected!'))
   .catch((err) => console.log(err));
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('server is on!');
 });
